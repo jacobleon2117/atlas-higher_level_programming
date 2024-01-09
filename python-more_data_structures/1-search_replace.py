19ad9267dcc220065c2f1e6e5868f3942b1ed785
Your Name <you@example.com>
2024-01-09 01:05:12 -0600

    fixing code

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,6 +4,6 @@ def search_replace(my_list, search, replace):
     my_list = [1, 2, 3, 4, 5, 4, 4, 4, 6]
     for idx, item in enumerate(my_list):
             if item == 4:
-                my_list[ix] = 44
+                my_list[idx] = 44
     print (my_list)
         
