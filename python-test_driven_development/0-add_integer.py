89d3c4477efcb029c480ef0f3640c2f482828377
Your Name <you@example.com>
2024-01-16 17:04:20 -0600

    Adding two integers as well checks input

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A module that adds two integers.
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    Args:
+        a: first int.
+        b: second int.
+    Return:
+        Sum of a and b.
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
