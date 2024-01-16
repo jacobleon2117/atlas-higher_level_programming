a0215ffdc79a19093a650af9c5118acc74fb85df
Your Name <you@example.com>
2024-01-16 16:28:28 -0600

    Fixed line 19 and docs

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,13 +7,13 @@ Module that defines a class square with an attribute size.
 
 class Square:
     """
-    Square class with an size attribute.
+    The Square class with a size attribute.
     """
     def __init__(self, size):
         """
-        Constructor for the Square class.
+        The constructor for the Square class.
         
         Parameters:
         size (int): The size of the square.
         """
-        self.size = size
+        self.__size = size
