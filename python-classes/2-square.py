9b25dc0cc9cea22702222b4cfdbb058457b719b4
Your Name <you@example.com>
2024-01-16 16:18:19 -0600

    Adding code/typererror/valueerror

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+
+"""
+Module that defines a class square with an attribute size.
+"""
+
+
+class square:
+    """
+    Square class with an attribute.
+    """
+    def __init__(self, size=0):
+        """
+        Constructor for the square class.
+        
+        Parameters:
+        size (int): Size of the square.
+        """
+        if type(size) is not int:
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueError("Size must be >= 0")
+        else:
+            self.size = size
