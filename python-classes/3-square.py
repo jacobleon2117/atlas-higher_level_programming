cac6d5df91795bf02f4969579140294315c7be55
Your Name <you@example.com>
2024-01-16 16:36:13 -0600

    Fixed line 20 and 22

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -17,9 +17,9 @@ class square:
         size (int): Size of the square.
         """
         if type(size) is not int:
-            raise TypeError("Size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("Size must be >= 0")
+            raise ValueError("size must be >= 0")
         else:
             self.size = size
 
