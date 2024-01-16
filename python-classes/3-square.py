be6ef3ee6e091727d39df9e192c2d6e45e1c5fe2
Your Name <you@example.com>
2024-01-16 16:20:49 -0600

    Def area(self) "finds the area"

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+
+"""
+Module that defines a class square with an attribute size.
+"""
+
+
+class square:
+    """
+    Square class with an attribute.
+    """
+    def __init__(self, size=0):
+        """
+        Constructor for the square class.
+        
+        Parameters:
+        size (int): Size of the square.
+        """
+        if type(size) is not int:
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueError("Size must be >= 0")
+        else:
+            self.size = size
+
+def area(self):
+    """
+    Method of the square class that finds the area.
+
+    Return:
+        the area of the sqaure.
+    """
+    return self.__size * self.__size
+        
