456b458a780cb5a363cb002dd00c4a55a33b78ee
Your Name <you@example.com>
2024-01-16 16:02:45 -0600

    Adding code

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+"""
+Module that defines a class square with an attribute size.
+"""
+
+
+class square:
+    """
+    Square class with an attribute.
+    """
+    def __init__(self, size):
+        """
+        Constructor for the square class.
+        
+        Parameters:
+        size (int): Size of the square.
+        """
+        self.size = size
