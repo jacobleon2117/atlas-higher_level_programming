819287bc0044524fa7f35065ae60840aa2e365ea
Your Name <you@example.com>
2024-01-16 16:40:35 -0600

    adding @property

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+
+"""
+This module defines a class Square with an attribute size.
+"""
+
+
+class Square:
+    """
+    The Square class with a size attribute.
+    """
+    def __init__(self, size=0):
+        """
+        The constructor for the Square class.
+
+        Parameters:
+        size (int): The size of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Getter for size attribute.
+        """
+        return self.__size
