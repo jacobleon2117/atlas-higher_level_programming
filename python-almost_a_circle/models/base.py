72650d84a37eaccf651551f730f440b3ef81fe4a
jacobleon2117 <jacobleon2117@gmail.com>
2024-01-31 09:56:28 -0600

    Added class Base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,9 @@
+class Base:
+    __nb__objects = 0
+    
+def __init__(self, id=None):
+    if id is not None:
+        self.id = id
+    else:
+        Base.__nb__objects += 1
+        self.id = self.__nb_objects
