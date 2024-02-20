4614899e902be735f4a05596b00969b4556d3e12
jacobleon2117 <jacobleon2117@gmail.com>
2024-02-20 14:47:47 -0600

    adding rest of files

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- update an incorrect score
+UPDATE second_table SET score = 10 WHERE name = "Bob";
---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,2 @@
+-- remove records with too low of a score
+DELETE FROM second_table WHERE score <= 5;
