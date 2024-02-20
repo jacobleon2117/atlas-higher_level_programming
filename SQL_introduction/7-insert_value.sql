2f17ad7440fccfc9fa04f746fd70735891156673
jacobleon2117 <jacobleon2117@gmail.com>
2024-02-20 14:43:41 -0600

    adding 7-8

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- insert new values into a table
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- count all records with an id of 89
+SELECT COUNT(*) FROM first_table WHERE ID = 89;
