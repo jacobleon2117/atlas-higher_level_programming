f87f05be89543b630bce67c3588e4f1b07e45db0
Your Name <you@example.com>
2024-03-26 01:06:48 -0500

    fixing export issues/newlines

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,20 +1,21 @@
 #!/usr/bin/node
 
-const ParentSquare = require('./5-square');
+const Rectangle = require('./4-rectangle');
 
-class Square extends ParentSquare {
-    constructor(size) {
-        super(size);
-    }
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
 
-    charPrint(c) {
-        if (c === undefined) {
-            c = 'X';
-        }
-        for (let i = 0; i < this.size; i++) {
-            console.log(c.repeat(this.size));
-        }
+  charPrint (c = 'X') {
+    for (let y = 0; y < this.height; y++) {
+      let row = '';
+      for (let x = 0; x < this.width; x++) {
+        row += c;
+      }
+      console.log(row);
     }
+  }
 }
 
 module.exports = Square;
