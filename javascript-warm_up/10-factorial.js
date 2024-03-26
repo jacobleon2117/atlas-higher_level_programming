039f2644762cb480a11efa8dacacbf322f35a848
Your Name <you@example.com>
2024-03-26 01:40:04 -0500

    fixing

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
-const factorial = (n) => {
+function factorial(n) {
     if (isNaN(n) || n === 0) {
         return 1;
     }
     return n * factorial(n - 1);
-};
+}
 
 const arg = parseInt(process.argv[2]);
-
 const result = factorial(arg);
-
 console.log(result);
