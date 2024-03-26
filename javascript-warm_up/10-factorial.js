181f1e47efa34c39167af13ca7a1aef4c9fa16d7
Your Name <you@example.com>
2024-03-26 01:45:19 -0500

    spacing

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 function factorial (n) {
-    if (isNaN(n) || n === 0) {
-        return 1;
-    }
-    return n * factorial(n - 1);
+  if (isNaN(n) || n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
 const arg = parseInt(process.argv[2]);
