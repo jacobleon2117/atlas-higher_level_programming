1ba48c9d4201350cc042b520303aa8c2a0c66830
Your Name <you@example.com>
2024-03-26 01:24:10 -0500

    fixing semistandard

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
 exports.logMe = function(item) {
-  if (typeof exports.logMe.counter === 'undefined') {
-    exports.logMe.counter = 0;
-  }
-    
-  console.log(exports.logMe.counter + ': ' + item);
-
-  exports.logMe.counter++;
-};
+    if (typeof exports.logMe.counter === 'undefined') {
+      exports.logMe.counter = 0;
+    }
+      
+    console.log(exports.logMe.counter + ': ' + item);
+  
+    exports.logMe.counter++;
+  };
+  
