f0f8667b0917e94e01b18e3272095caeeabbc010
Your Name <you@example.com>
2024-03-26 01:42:59 -0500

    whitespace fixing

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,14 +2,14 @@
 
 const args = process.argv.slice(2).map(Number);
 const findSecondLargest = (arr) => {
-    if (arr.length < 2) {
-        return 0;
-    }
-    
-    const sortedArr = arr.sort((a, b) => b - a);
-    
-    let secondLargest = sortedArr[1];
-    
+  if (arr.length < 2) {
+    return 0;
+  }
+
+  const sortedArr = arr.sort((a, b) => b - a);
+
+  let secondLargest = sortedArr[1];
+
     return secondLargest;
 };
 
