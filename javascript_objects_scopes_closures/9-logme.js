22f7bb52a4faf92bb0502e17f399fb0410ce229f
Your Name <you@example.com>
2024-03-26 01:24:49 -0500

    fixing

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-exports.logMe = function(item) {
+exports.logMe = function (item) {
     if (typeof exports.logMe.counter === 'undefined') {
       exports.logMe.counter = 0;
     }
-      
+    
     console.log(exports.logMe.counter + ': ' + item);
   
     exports.logMe.counter++;
