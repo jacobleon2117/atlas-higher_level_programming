b3b6e937858d34fb78f5de1de2076c1589af83e6
Your Name <you@example.com>
2024-03-26 01:00:34 -0500

    fixing #5

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,32 +1,12 @@
 #!/usr/bin/node
 
-class Rectangle {
-    constructor(w, h) {
-        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-            return {};
-        }
-        this.width = w;
-        this.height = h;
-    }
+// 5-square.js
+const Rectangle = require('./4-rectangle.js');
 
-    print() {
-        for (let i = 0; i < this.height; i++) {
-            let row = '';
-            for (let j = 0; j < this.width; j++) {
-                row += 'X';
-            }
-            console.log(row);
-        }
-    }
-
-    rotate() {
-        [this.width, this.height] = [this.height, this.width];
-    }
-
-    double() {
-        this.width *= 2;
-        this.height *= 2;
+class Square extends Rectangle {
+    constructor(size) {
+        super(size, size); // Call the constructor of Rectangle with size for both width and height
     }
 }
 
-module.exports = Rectangle;
+module.exports = Square;
